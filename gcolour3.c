b35f4aa97f4b28dab96ac94c66f265d38f518281
stevenhoneyman <stevenhoneyman@users.noreply.github.com>
2014-06-08 20:06:47 +0100

    Added "gcolour3.c"
    A replacement for gcolor2 which does all I need it to (dropper tool thing).
    
    Signed-off-by: stevenhoneyman <stevenhoneyman@users.noreply.github.com>

---
+++ gcolour3.c
@@ -0,0 +1,15 @@
+/*
+   gcolour3 - "I don't care if it's considered `deprecated`; the new version SUCKS!"
+   v1.0 2014-06-08 Steven Honeyman <stevenhoneyman at gmail com>
+
+   Compile using: gcc gcolour3.c -Os -s `pkg-config gtk+-3.0 --cflags` `pkg-config gtk+-3.0 --libs` -o gcolour3
+*/
+
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[])
+{
+  gtk_init (&argc, &argv);
+  GtkWidget *dialog = gtk_color_selection_dialog_new("Colour Selection Dialog");
+  return gtk_dialog_run(GTK_DIALOG(dialog));
+}

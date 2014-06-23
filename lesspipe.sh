6c3b002e51eda3f56a8e39dfe65ce5064380309d
Steven Honeyman <stevenhoneyman@users.noreply.github.com>
2014-06-23 20:46:10 +0100

    neatened lesspipe.sh up. also added manpage support

--- lesspipe.sh
+++ lesspipe.sh
@@ -1,31 +1,45 @@
 #!/bin/sh
 #
-# lesspipe.sh - auto highlight syntax in less.
-# 2014-06-21 Steven Honeyman <stevenhoneyman at gmail com>
+# lesspipe.sh - preprocessor for less.
+# 2014-06-23 Steven Honeyman <stevenhoneyman at gmail com>
 #
 # Usage: in ~/.bashrc (etc), add:
 #   export LESSOPEN="|- lesspipe.sh %s"
 #
 
+SRC_HLT_CMD="source-highlight --failsafe --tab=4 -f esc256-steven --style-file=esc256.style "
+
+show_unknown() {
+    file "$1"
+    file "$1" --mime
+    echo
+    xxd -c 20 "$1"
+}
+
 if [ -t 0 ]; then
   for source in "$@"; do
-	if file -b --mime-type "$source" | grep -q 'text'; then
-		case $source in
-		*ChangeLog|*changelog)
-				source-highlight --failsafe -f esc256 --lang-def=changelog.lang --style-file=esc256.style -i "$source" ;;
-		*Makefile|*makefile)
-				source-highlight --failsafe -f esc256 --lang-def=makefile.lang --style-file=esc256.style -i "$source" ;;
-		*.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|\
-			*.ad[asb]|*.asm|*.inc|*.[ch]|*.[ch]pp|*.[ch]xx|*.cc|*.hh|\
-			*.lsp|*.l|*.pas|*.p|*.xml|*.xps|*.xsl|*.axp|*.ppd|*.pov|\
-			*.py|*.rb|*.sql|*.ebuild|*.eclass|.bashrc|.bash_aliases|.bash_functions)
-			source-highlight --failsafe --infer-lang -f esc256 --style-file=esc256.style -n -i "$source" ;;
-			*)  source-highlight --failsafe --infer-lang -f esc256 --style-file=esc256.style -i "$source" ;;
-		esac
-	else
-		# without the check, "less --help" errors when xxd tries to open it
-		if [ -f "$source" ]; then xxd -c 20 "$source"; fi
-	fi
+    if [ ! -e "$source" ] && [ ! -z $(type -P "$source") ]; then
+	source=$(type -P "$source")
+    fi
+    if file -b --mime-type "$source" | grep -q 'text'; then
+	case $source in
+	    *ChangeLog|*changelog)  $SRC_HLT_CMD --lang-def=changelog.lang -i "$source" ;;
+	    *Makefile|*makefile)    $SRC_HLT_CMD --lang-def=makefile.lang -i "$source" ;;
+	    .bashrc|.bash_*)	    $SRC_HLT_CMD --lang-def=sh.lang -n -i "$source" ;;
+	    *.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|\
+		*.ad[asb]|*.asm|*.inc|*.[ch]|*.[ch]pp|*.[ch]xx|*.cc|*.hh|\
+		*.lsp|*.l|*.pas|*.p|*.xml|*.xps|*.xsl|*.axp|*.ppd|*.pov|\
+		*.py|*.rb|*.sql|*.ebuild|*.eclass)
+				    $SRC_HLT_CMD --infer-lang -n -i "$source" ;;
+	    *.1)  		    mandoc "$source" ;;
+	    *)    		    $SRC_HLT_CMD --infer-lang -i "$source" ;;
+	esac
+    elif echo "$source" | grep -q 'share/man/man./'; then
+	zcat "$source" | mandoc
+    else
+	# without the check, "less --help" errors when xxd tries to open it
+	[ -f "$source" ] && show_unknown "$source"
+    fi
   done
 else
     stdintemp=$(mktemp)
@@ -33,22 +47,17 @@ else
     MIME=$(file -b --mime-type $stdintemp)
     if echo $MIME | grep -q 'text'; then
 	case $MIME in
-	    text/x-c)
-		source-highlight --failsafe --lang-def=c.lang -f esc256 --style-file=esc256.style -n -i "$stdintemp" ;;
-	    text/x-shellscript)
-		source-highlight --failsafe --lang-def=sh.lang -f esc256 --style-file=esc256.style -n -i "$stdintemp" ;;
-	    text/x-diff)
-		source-highlight --failsafe --lang-def=diff.lang -f esc256 --style-file=esc256.style -i "$stdintemp" ;;
-	    text/x-python)
-		source-highlight --failsafe --lang-def=python.lang -f esc256 --style-file=esc256.style -n -i "$stdintemp" ;;
-	    text/x-perl)
-		source-highlight --failsafe --lang-def=perl.lang -f esc256 --style-file=esc256.style -n -i "$stdintemp" ;;
-	    text/x-sql)
-		source-highlight --failsafe --lang-def=sql.lang -f esc256 --style-file=esc256.style -n -i "$stdintemp" ;;
-	    *)  cat "$stdintemp" ;;
+	    text/x-c) 		$SRC_HLT_CMD --lang-def=c.lang -n -i "$stdintemp" ;;
+	    text/x-shellscript)	$SRC_HLT_CMD --lang-def=sh.lang -n -i "$stdintemp" ;;
+	    text/x-diff)	$SRC_HLT_CMD --lang-def=diff.lang -i "$stdintemp" ;;
+	    text/x-python)	$SRC_HLT_CMD --lang-def=python.lang -n -i "$stdintemp" ;;
+	    text/x-perl)	$SRC_HLT_CMD --lang-def=perl.lang -n -i "$stdintemp" ;;
+	    text/x-sql)		$SRC_HLT_CMD --lang-def=sql.lang -n -i "$stdintemp" ;;
+	    text/troff)  	mandoc "$stdintemp" ;;
+	    *)  		cat "$stdintemp" ;;
 	esac
     else
-        xxd -c 20 "$stdintemp"
+        show_unknown "$stdintemp"
     fi
     rm -f "$stdintemp"
 fi
